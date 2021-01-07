014c63bf127108c16a77da1292e96f67a0f17a0a
taegeun lim <imori333@gmail.com>
2021-01-07 20:57:50 +0900

    fix bug and complete

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ def convert_pixel_to_hex(filePath):
     colors = image.getpixel((0,0))
     print(colors, rgb_to_hex(colors))
     hex_pixel_list = []
-    for y in range(int(height / pixel_per_5mm)):
+    for y in range(round(height / pixel_per_5mm)):
         row = []
-        for x in range(int(width / pixel_per_5mm)):
-            rgb_colors = image.getpixel((int(x * pixel_per_5mm),int( y * pixel_per_5mm)))
+        for x in range(round(width / pixel_per_5mm)):
+            rgb_colors = image.getpixel((round(x * pixel_per_5mm + 15),round( y * pixel_per_5mm + 15)))
             row.append(rgb_to_hex(rgb_colors))
         hex_pixel_list.append(row)
     return (hex_pixel_list, image.size, pixel_per_5mm)
@@ -36,13 +36,12 @@ def convert_pixel_to_hex(filePath):
 
 def drawImage(hex_list, canvasSize, filePath, pixel_per_5mm):
     img = Image.new('RGB', (canvasSize[0], canvasSize[1]), color=(255, 255, 255))
-    fnt = ImageFont.truetype('NotoSans-Regular.ttf', 10)
+    fnt = ImageFont.truetype('NotoSans-Regular.ttf', 11)
     d = ImageDraw.Draw(img)
-    d.text((10,10), 'ffffff', font=fnt, fill=(0,0,0))
     for y_idx, y_value in enumerate(hex_list):
         for x_idx, x_value in enumerate(y_value):
-            d.text((int(x_idx * pixel_per_5mm), int(y_idx * pixel_per_5mm)), x_value, font=fnt, fill=(0, 0, 0))
-
+            d.text((round(x_idx * pixel_per_5mm + 15), round(y_idx * pixel_per_5mm + 15)), x_value, font=fnt, fill=(0, 0, 0))
+    pathlib.Path("./hex").mkdir(parents=True, exist_ok=True)
     img.save('./hex/' + filePath )
 
 def rgb_to_hex(rgb):
@@ -52,8 +51,8 @@ def rgb_to_hex(rgb):
 if __name__ == '__main__':
     images = get_image()
     print(images)
-    hex_list, size, pixel_p_5m = convert_pixel_to_hex(images[0])
-    print(hex_list, size, pixel_p_5m)
-    drawImage(hex_list, size, images[0], pixel_p_5m)
-    # print(list)
+    for image in images:
+        hex_list, size, pixel_p_5m = convert_pixel_to_hex(image)
+        print(len(hex_list), len(hex_list[0]), size, pixel_p_5m)
+        drawImage(hex_list, size, image, pixel_p_5m)
 
